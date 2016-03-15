494fc8ff5e0d11b9181b7857eadfcff584fc5fad
noyobo <nongyoubao@alibaba-inc.com>
2016-03-15 16:24:28 +0800

    test: update

--- test/index-test.js
+++ test/index-test.js
@@ -3,11 +3,6 @@ var path = require('path');
 var data = require('../files/data.js');
 var fs = require('fs');
 
-// data.map(function(item) {
-//   item.imageUrl = path.join(__dirname, '../', item.imageUrl);
-//   return item;
-// })
-
 imgSpriter.processHTTP(data)
   .then(function(data) {
     imgSpriter.sprite(data, {
@@ -21,7 +16,7 @@ imgSpriter.processHTTP(data)
         var spriteOutput = path.join(__dirname, './sprite.png');
 
         fs.writeFileSync(
-          './test/index-output.json',
+          path.join(__dirname, './index-output.json'),
           JSON.stringify(data.dataSource, null, 2)
         );
         data.streamPNG8
--- test/readImageInfoByHTTP-test.js
+++
@@ -1,7 +0,0 @@
-var png = require('../lib/png.js');
-
-
-png.readInfoByHTTP('http://img.alicdn.com/tps/i2/TB1fvaMLVXXXXbuXFXXEDhGGXXX-32-32.png')
-  .then(function(imginfo) {
-    console.log(imginfo);
-  })
