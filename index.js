1f0552d25dad49a22f307f51286bf8eb0b04e334
noyobo <noyobo@gmail.com>
2019-01-16 16:30:35 +0800

    feat: node 8  & upgrade deps

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 'use strict';
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
-
 const debug = require('debug')('img-spriter');
-const assert = require('assert');
-const path = require('path');
-const fs = require('fs');
-
 const merge = require('object-merge');
 const Pngquant = require('pngquant');
+
 const GrowingPacker = require('./lib/GrowingPacker.js');
 const png = require('./lib/png.js');
 
 const defaultOptions = {
   margin: 5,
   png8: false,
-  dpi: 1
+  dpi: 1,
 };
 
 function wrap(item, imageInfo) {
@@ -23,7 +17,7 @@ function wrap(item, imageInfo) {
 
   item.sizeDiff = {
     w: [item.w !== imageInfo.width, item.w, imageInfo.width],
-    h: [item.h !== imageInfo.height, item.h, imageInfo.height]
+    h: [item.h !== imageInfo.height, item.h, imageInfo.height],
   };
 
   item.w = imageInfo.width;
@@ -32,25 +26,29 @@ function wrap(item, imageInfo) {
 }
 
 function getImageInfo(arr) {
-  return Promise.all(arr.map(function (item, key) {
-    if (!item.imageUrl) {
-      throw new Error('imageUrl is required!');
-    }
-    return png.readInfo(item.imageUrl).then(imageInfo => {
-      return wrap(item, imageInfo);
-    });
-  }));
+  return Promise.all(
+    arr.map(function(item, key) {
+      if (!item.imageUrl) {
+        throw new Error('imageUrl is required!');
+      }
+      return png.readInfo(item.imageUrl).then((imageInfo) => {
+        return wrap(item, imageInfo);
+      });
+    }),
+  );
 }
 
 function getImageInfoByHTTP(arr) {
-  return Promise.all(arr.map(function (item, key) {
-    if (!item.imageUrl) {
-      throw new Error('imageUrl is required!');
-    }
-    return png.readInfoByHTTP(item.imageUrl).then(imageInfo => {
-      return wrap(item, imageInfo);
-    });
-  }));
+  return Promise.all(
+    arr.map(function(item, key) {
+      if (!item.imageUrl) {
+        throw new Error('imageUrl is required!');
+      }
+      return png.readInfoByHTTP(item.imageUrl).then((imageInfo) => {
+        return wrap(item, imageInfo);
+      });
+    }),
+  );
 }
 
 module.exports = {
@@ -63,138 +61,112 @@ module.exports = {
    * @param  {Object} options    参数
    * @return {Stream}
    */
-  sprite: (() => {
-    var ref = _asyncToGenerator(function* (imageFrames, options) {
-      options = merge(defaultOptions, options || {});
+  sprite: async function spriter(imageFrames, options) {
+    options = merge(defaultOptions, options || {});
 
-      debug('options:', options);
+    debug('options:', options);
 
-      options.margin = options.margin * options.dpi;
+    options.margin = options.margin * options.dpi;
 
-      var packer = new GrowingPacker();
-      // 排序图片
-      // TODO: 支持更多排序算法
-      imageFrames.sort(function (a, b) {
-        return b.w * b.h - a.w * a.h;
-      });
+    var packer = new GrowingPacker();
+    // 排序图片
+    // TODO: 支持更多排序算法
+    imageFrames.sort(function(a, b) {
+      return b.w * b.h - a.w * a.h;
+    });
 
-      // 图标的间隔
-      imageFrames.map(function (a) {
-        a.w = a.w + options.margin;
-        a.h = a.h + options.margin;
-        return a;
-      });
+    // 图标的间隔
+    imageFrames.map(function(a) {
+      a.w = a.w + options.margin;
+      a.h = a.h + options.margin;
+      return a;
+    });
 
-      packer.fit(imageFrames);
+    packer.fit(imageFrames);
 
-      const outputPNG = png.create(packer.root.w, packer.root.h);
+    const outputPNG = png.create(packer.root.w, packer.root.h);
 
-      imageFrames = imageFrames.map(function (imageObj) {
-        var imageInfo = imageObj.imageInfo;
-        var image = imageInfo.image;
-        try {
-          image.bitblt(outputPNG, 0, 0, image.width, image.height, imageObj.fit.x, imageObj.fit.y);
-        } catch (err) {
-          debug(err);
-          throw err;
-        }
+    imageFrames = imageFrames.map(function(imageObj) {
+      var imageInfo = imageObj.imageInfo;
+      var image = imageInfo.image;
+      try {
+        image.bitblt(outputPNG, 0, 0, image.width, image.height, imageObj.fit.x, imageObj.fit.y);
+      } catch (err) {
+        debug(err);
+        throw err;
+      }
 
-        console.log(imageObj);
-
-        var frame = {
-          'frame': {
-            'y': imageObj.fit.y * -1 / options.dpi,
-            'x': imageObj.fit.x * -1 / options.dpi,
-            'w': (imageObj.w - options.margin) / options.dpi,
-            'h': (imageObj.h - options.margin) / options.dpi
-          },
-          'sourceSize': {
-            'h': image.height,
-            'w': image.width
-          }
-        };
-
-        delete imageObj.imageInfo;
-        delete imageObj.fit;
-        delete imageObj.w;
-        delete imageObj.h;
-        Object.assign(frame, imageObj);
-        return frame;
-      });
-
-      // Starts converting data to PNG file Stream.
-      outputPNG.pack();
-
-      const result = {
-        stream: outputPNG,
-        dataSource: {
-          'frames': imageFrames,
-          'meta': {
-            'height': outputPNG.height / options.dpi,
-            'width': outputPNG.width / options.dpi,
-            'originWidth': outputPNG.width,
-            'originHeight': outputPNG.height,
-            'hash': Date.now().toString(32),
-            'dpi': options.dpi
-          },
-          'options': options
-        }
+      var frame = {
+        frame: {
+          y: (imageObj.fit.y * -1) / options.dpi,
+          x: (imageObj.fit.x * -1) / options.dpi,
+          w: (imageObj.w - options.margin) / options.dpi,
+          h: (imageObj.h - options.margin) / options.dpi,
+        },
+        sourceSize: {
+          h: image.height,
+          w: image.width,
+        },
       };
 
-      if (options.png8) {
-        // png24 converter png8
-        const outputPNG8 = png.create(packer.root.w, packer.root.h);
-
-        outputPNG.bitblt(outputPNG8, 0, 0, packer.root.w, packer.root.h, 0, 0);
-
-        outputPNG8.pack();
-
-        result.streamPNG8 = outputPNG8.pipe(new Pngquant());
-      }
-
-      return result;
+      delete imageObj.imageInfo;
+      delete imageObj.fit;
+      delete imageObj.w;
+      delete imageObj.h;
+      Object.assign(frame, imageObj);
+      return frame;
     });
 
-    function spriter(_x, _x2) {
-      return ref.apply(this, arguments);
+    // Starts converting data to PNG file Stream.
+    outputPNG.pack();
+
+    const result = {
+      stream: outputPNG,
+      dataSource: {
+        frames: imageFrames,
+        meta: {
+          height: outputPNG.height / options.dpi,
+          width: outputPNG.width / options.dpi,
+          originWidth: outputPNG.width,
+          originHeight: outputPNG.height,
+          hash: Date.now().toString(32),
+          dpi: options.dpi,
+        },
+        options: options,
+      },
+    };
+
+    if (options.png8) {
+      // png24 converter png8
+      const outputPNG8 = png.create(packer.root.w, packer.root.h);
+
+      outputPNG.bitblt(outputPNG8, 0, 0, packer.root.w, packer.root.h, 0, 0);
+
+      outputPNG8.pack();
+
+      result.streamPNG8 = outputPNG8.pipe(new Pngquant());
     }
 
-    return spriter;
-  })(),
-  process: (() => {
-    var ref = _asyncToGenerator(function* (imageFrames) {
-      if (!imageFrames || imageFrames.length === 0) {
-        throw new Error('icorrect argument imageFrames!');
-      }
-      imageFrames = yield getImageInfo(imageFrames).catch(function (err) {
-        debug(err);
-        throw new Error(err);
-      });
-      return imageFrames;
+    return result;
+  },
+  process: async function process(imageFrames) {
+    if (!imageFrames || imageFrames.length === 0) {
+      throw new Error('icorrect argument imageFrames!');
+    }
+    imageFrames = await getImageInfo(imageFrames).catch((err) => {
+      debug(err);
+      throw new Error(err);
     });
-
-    function process(_x3) {
-      return ref.apply(this, arguments);
+    return imageFrames;
+  },
+  processHTTP: async function processHTTP(imageFrames) {
+    if (!imageFrames || imageFrames.length === 0) {
+      throw new Error('icorrect argument imageFrames!');
     }
-
-    return process;
-  })(),
-  processHTTP: (() => {
-    var ref = _asyncToGenerator(function* (imageFrames) {
-      if (!imageFrames || imageFrames.length === 0) {
-        throw new Error('icorrect argument imageFrames!');
-      }
-      imageFrames = yield getImageInfoByHTTP(imageFrames).catch(function (err) {
-        debug(err);
-        throw new Error(err);
-      });
-      return imageFrames;
+    imageFrames = await getImageInfoByHTTP(imageFrames).catch((err) => {
+      debug(err);
+      throw new Error(err);
     });
-
-    function processHTTP(_x4) {
-      return ref.apply(this, arguments);
-    }
-
-    return processHTTP;
-  })()
+    return imageFrames;
+  },
 };
--- test/index-test.js
+++ test/index-test.js
@@ -1,37 +1,32 @@
-var imgSpriter = require('../')
+var imgSpriter = require('../');
 var path = require('path');
 var data = require('../files/data.js');
 var fs = require('fs');
 
-imgSpriter.processHTTP(data)
+imgSpriter
+  .processHTTP(data)
   .then(function(data) {
-    imgSpriter.sprite(data, {
+    imgSpriter
+      .sprite(data, {
         margin: 5,
-        png8:true
+        png8: true,
       })
       .then(function(data) {
-
-
         var spriteOutput8 = path.join(__dirname, './sprite-8.png');
         var spriteOutput = path.join(__dirname, './sprite.png');
 
-        fs.writeFileSync(
-          path.join(__dirname, './index-output.json'),
-          JSON.stringify(data.dataSource, null, 2)
-        );
-        data.streamPNG8
-          .pipe(fs.createWriteStream(spriteOutput8))
-          .on('finish', function() {
-            console.log('>>Output image:', spriteOutput8);
-          });
-        data.stream
-          .pipe(fs.createWriteStream(spriteOutput))
-          .on('finish', function() {
-            console.log('>>Output image:', spriteOutput);
-          });
-      }).catch(function(err) {
-        console.log('========', err);
+        fs.writeFileSync(path.join(__dirname, './index-output.json'), JSON.stringify(data.dataSource, null, 2));
+        data.streamPNG8.pipe(fs.createWriteStream(spriteOutput8)).on('finish', function() {
+          console.log('>>Output image:', spriteOutput8);
+        });
+        data.stream.pipe(fs.createWriteStream(spriteOutput)).on('finish', function() {
+          console.log('>>Output image:', spriteOutput);
+        });
       })
-  }).catch(function(err) {
-    console.log('========', err);
+      .catch(function(err) {
+        console.log('========', err);
+      });
   })
+  .catch(function(err) {
+    console.log('========', err);
+  });
